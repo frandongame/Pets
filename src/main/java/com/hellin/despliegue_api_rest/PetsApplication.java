32e842d46f0be58d1eae41c205ab5539b887e482
frandongame <frandongame@gmail.com>
2025-09-29 10:38:18 +0200

    estructura del proyecto

---
+++ src/main/java/com/hellin/despliegue_api_rest/PetsApplication.java
@@ -0,0 +1,13 @@
+package com.hellin.despliegue_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsApplication.class, args);
+	}
+
+}
